457905cf8a5f511542ec690653804be1378f3191
Tapan Chandra <tapanc.nallan@gmail.com>
2018-02-14 02:34:29 +0530

    Initial Commit

---
+++ src/main/java/com/invia/interview/cashpoolapplication/CashpoolApplication.java
@@ -0,0 +1,12 @@
+package com.invia.interview.cashpoolapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashpoolApplication.class, args);
+	}
+}
---
+++ src/test/java/com/invia/interview/cashpoolapplication/CashpoolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.invia.interview.cashpoolapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CashpoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
